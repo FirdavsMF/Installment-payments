c2ed5201984f6d4311fbd050d1d24dc648557c6c
Firdavs <firdavs.2001.fed@gmail.com>
2021-09-24 22:09:17 +0500

    add new func SendEmail

--- cmd/billing/main.go
+++ cmd/billing/main.go
@@ -1,9 +1,31 @@
 package main
+
 import (
-	// "github.com/FirdavsMF/Installment-payments/pkg/billing"
-	// "fmt"
+	"fmt"
+
+	"github.com/FirdavsMF/Installment-payments/pkg/product"
+
 )
 func main() {
-
+	var category string
+	var price int64
+	var months int64
+	var gmail string
+	fmt.Print("Введите категория товар (smartphone, computer, TV) : ")
+    fmt.Scan(&category)
+	fmt.Print("Введите сумма товар: ")
+    fmt.Scan(&price) 
+    fmt.Print("Введите диапазон ")
+    fmt.Scan(&months)
+	fmt.Print("Введите диапазон ")
+    fmt.Scan(&gmail)
+	text := ""
+    t := product.SendEmail(gmail, text)
+	// if err := product.SendEmail(gmail, text); err != nil {
+	// 	panic(err)
+	// }
+    resault, err := product.Calc(months, category, price)
+	fmt.Println(resault, err)
 	
 }
+
