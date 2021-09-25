9c20a3732433ab2eb9854b0689dd86ed1cbbd8d6
Firdavs <firdavs.2001.fed@gmail.com>
2021-09-25 09:06:09 +0500

    add new func Gmail

--- cmd/billing/main.go
+++ cmd/billing/main.go
@@ -10,17 +10,17 @@ func main() {
 	var category string
 	var price int64
 	var months int64
-	var gmail string
+	// var gmail string
 	fmt.Print("Введите категория товар (smartphone, computer, TV) : ")
     fmt.Scan(&category)
 	fmt.Print("Введите сумма товар: ")
     fmt.Scan(&price) 
     fmt.Print("Введите диапазон ")
     fmt.Scan(&months)
-	fmt.Print("Введите диапазон ")
-    fmt.Scan(&gmail)
+	// fmt.Print("Введите электроний почта Gmail ")
+    // fmt.Scan(&gmail)
     resault, err := product.Calc(months, category, price)
-	product.Gmail(category, months, price, gmail, resault )
+	// product.Gmail(category, months, price, gmail, resault )
     
 	fmt.Println(resault, err)
 	
